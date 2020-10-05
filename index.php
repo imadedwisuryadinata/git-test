0c7cd7a32e87298cc4490aca67feecf605388218
imadedwisuryadinata <dwi.suryadinata@gmail.com>
2020-10-05 11:41:15 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1> Hello World </h1>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
